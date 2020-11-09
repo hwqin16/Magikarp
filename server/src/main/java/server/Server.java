a803b1592d712bd5daea8d207355daab60c92951
Charlie Summers <charlie@gomerits.com>
2020-11-08 17:26:02 -0800

    Add initial server module

---
+++ server/src/main/java/server/Server.java
@@ -0,0 +1,10 @@
+package server;
+
+import io.javalin.Javalin;
+
+public class Server {
+    public static void main(String[] args) {
+        Javalin app = Javalin.create().start(7000);
+        app.get("/", ctx -> ctx.result("Hello World"));
+    }
+}
