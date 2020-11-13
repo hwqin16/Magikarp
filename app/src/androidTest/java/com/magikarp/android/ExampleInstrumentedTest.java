f92d4b1a53e12639d8b6d7e544ad9335168bfe5a
Wade Johnson <wmj2108@columbia.edu>
2020-11-12 18:52:01 -0800

    Corrected package name typo

--- app/src/androidTest/java/com/magikarp/android/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/magikarp/android/ExampleInstrumentedTest.java
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
     public void useAppContext() {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
-        assertEquals("edu.columbia.magikarp", appContext.getPackageName());
+        assertEquals("com.magikarp.android", appContext.getPackageName());
     }
 }
