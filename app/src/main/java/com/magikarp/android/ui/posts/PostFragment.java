378ea1f406a1c39e65faf3321568a7cf3ccc2a66
Nick Clark <nicholaus.clark115@gmail.com>
2020-11-17 16:42:43 -0500

    Fetch GPS location on click of GPS button (#23)

--- app/src/main/java/com/magikarp/android/ui/posts/PostFragment.java
+++ app/src/main/java/com/magikarp/android/ui/posts/PostFragment.java
@@ -1,15 +1,21 @@
 package com.magikarp.android.ui.posts;
 
 import android.app.Activity;
+import android.content.ComponentName;
+import android.content.Context;
 import android.content.Intent;
+import android.content.ServiceConnection;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.location.Location;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.IBinder;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.EditText;
@@ -18,6 +24,7 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import com.magikarp.android.R;
+import com.magikarp.android.services.LocationService;
 import java.io.FileNotFoundException;
 import java.io.InputStream;
 
@@ -37,6 +44,25 @@ public class PostFragment extends Fragment {
   private double longitude = Double.NEGATIVE_INFINITY;
   private String imagePath = null;
   private String content = null;
+
+  private LocationService gpsService;
+  private boolean isGpsServiceBound = false;
+  private ServiceConnection gpsServiceConnection = new ServiceConnection() {
+    @Override
+    public void onServiceConnected(ComponentName name, IBinder service) {
+      final LocationService.LocationServiceBinder binder =
+          (LocationService.LocationServiceBinder) service;
+      gpsService = binder.getService();
+      Log.d("GPS Service Conn", "Connected");
+      isGpsServiceBound = true;
+    }
+
+    @Override
+    public void onServiceDisconnected(ComponentName name) {
+      Log.d("GPS Service Conn", "Disconnected");
+      isGpsServiceBound = false;
+    }
+  };
 
   /**
    * Use this factory method to create a new instance of
@@ -59,6 +85,17 @@ public class PostFragment extends Fragment {
   public void onCreate(final Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setHasOptionsMenu(true);
+
+    final Intent gpsIntent = new Intent(getContext(), LocationService.class);
+    getActivity().bindService(gpsIntent, gpsServiceConnection, Context.BIND_AUTO_CREATE);
+    getActivity().startService(gpsIntent);
+  }
+
+  @Override
+  public void onDestroy() {
+    gpsService.dispose();
+    getActivity().unbindService(gpsServiceConnection);
+    super.onDestroy();
   }
 
   @Override
@@ -66,6 +103,7 @@ public class PostFragment extends Fragment {
     super.onCreateOptionsMenu(menu, inflater);
     if (getArguments().getBoolean(getResources().getString(R.string.args_is_editable))) {
       inflater.inflate(R.menu.menu_post_edit, menu);
+      menu.findItem(R.id.menu_get_location).setOnMenuItemClickListener(this::onGpsButtonClick);
     } else {
       inflater.inflate(R.menu.menu_post_view, menu);
     }
@@ -81,9 +119,6 @@ public class PostFragment extends Fragment {
   public void onViewCreated(@NonNull final View view, @Nullable final Bundle savedInstanceState) {
     final ImageView imageView = view.findViewById(R.id.create_post_image_preview);
     imageView.setOnClickListener(this::selectImageAction);
-
-    // final Button postButton = getLayoutInflater().inflate(R.layout.fragment_post_toolbar_extension, null).findViewById(R.id.post_content);
-    // postButton.setOnClickListener(this::postAction);
 
     if (savedInstanceState != null) {
       imagePath = savedInstanceState.getString(IMAGE_VIEW_ARGUMENT);
@@ -105,10 +140,32 @@ public class PostFragment extends Fragment {
     bundle.putDouble(LATITUDE_ARGUMENT, latitude);
   }
 
+  @Override
+  public void onActivityResult(final int requestCode, final int resultCode, final Intent data) {
+    super.onActivityResult(requestCode, resultCode, data);
+    if (resultCode == Activity.RESULT_OK) {
+      if (requestCode == RESULT_LOAD_IMG) {
+        loadImage(data.getData());
+      } else {
+        throw new IllegalStateException("Unexpected value: " + requestCode);
+      }
+    }
+  }
+
+  /**
+   * Action to execute on post button press.
+   *
+   * @param view action source
+   */
   private void postAction(final View view) {
     Log.d("postAction", "Entered");
   }
 
+  /**
+   * Starts the activity to select an image.
+   *
+   * @param view action source
+   */
   private void selectImageAction(final View view) {
     Log.d("selectImageAction", "Entered");
     Intent photoSelectionIntent = new Intent(Intent.ACTION_PICK);
@@ -116,18 +173,11 @@ public class PostFragment extends Fragment {
     startActivityForResult(photoSelectionIntent, RESULT_LOAD_IMG);
   }
 
-  @Override
-  public void onActivityResult(final int requestCode, final int resultCode, final Intent data) {
-    super.onActivityResult(requestCode, resultCode, data);
-    if (resultCode == Activity.RESULT_OK) {
-      switch (requestCode) {
-        case RESULT_LOAD_IMG: {
-          loadImage(data.getData());
-        }
-      }
-    }
-  }
-
+  /**
+   * Loads an image from a URI into the image view.
+   *
+   * @param imageUri image to load
+   */
   private void loadImage(final Uri imageUri) {
     try {
       final InputStream imageInput = getContext().getContentResolver().openInputStream(imageUri);
@@ -139,6 +189,22 @@ public class PostFragment extends Fragment {
     } catch (final FileNotFoundException e) {
       Log.e("onActivityResult", "Failed to load image.", e);
     }
+  }
+
+  /**
+   * GPS button click callback.
+   *
+   * @param item item clicked
+   * @return always {@code true}
+   */
+  private boolean onGpsButtonClick(final MenuItem item) {
+    if (isGpsServiceBound) {
+      final Location location = gpsService.getLocation();
+      longitude = location.getLongitude();
+      latitude = location.getLatitude();
+      Log.d("onGpsButtonClick", "Found GPS Location: " + latitude + ", " + longitude);
+    }
+    return true;
   }
 
 }
