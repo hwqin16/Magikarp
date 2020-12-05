ac25067c14a94eb779f04507e84c15b133e58be5
Nicholaus Clark <nicholaus.clark115@gmail.com>
2020-12-05 13:00:51 -0500

    Added caption maximum length.

--- app/src/main/java/com/magikarp/android/ui/posts/PostFragment.java
+++ app/src/main/java/com/magikarp/android/ui/posts/PostFragment.java
@@ -7,7 +7,6 @@ import android.content.ContentResolver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
-import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.location.Location;
 import android.net.Uri;
